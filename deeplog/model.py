e65df2b06516cf809c04e88133b9457422959ba5
iyunbo <yunbo.wang@solven.eu>
2020-03-01 18:25:48 +0100

    initial commit

---
+++ deeplog/model.py
@@ -0,0 +1,20 @@
+import torch
+import torch.nn as nn
+
+
+class DeepLog(nn.Module):
+    def __init__(self, input_size, hidden_size, num_layers, num_keys, device):
+        super(DeepLog, self).__init__()
+        self.device = device
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
+        self.fc = nn.Linear(hidden_size, num_keys)
+        self.to(self.device)
+
+    def forward(self, x):
+        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(self.device)
+        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(self.device)
+        out, _ = self.lstm(x, (h0, c0))
+        out = self.fc(out[:, -1, :])
+        return out
