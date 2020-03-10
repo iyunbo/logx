4bda77c59abac4335550c11ed00d64d0081ca02d
iyunbo <yunbo.wang@solven.eu>
2020-03-10 12:58:17 +0100

    update paths and requirements.txt

--- workflow/pipeline.py
+++ workflow/pipeline.py
@@ -8,7 +8,7 @@ log_file = 'log4j-2020-02-28-22.log'  # The input log file name
 log_format = '<Date> <Time> <Level> <Component>: <Content>'  # HDFS log format
 # Regular expression list for optional preprocessing (default: [])
 regex = []
-st = 0.5  # Similarity threshold
+st = 0.6  # Similarity threshold
 depth = 4  # Depth of all leaf nodes
 
 parser = Drain.LogParser(log_format, indir=input_dir, outdir=output_dir, depth=depth, st=st, rex=regex)
