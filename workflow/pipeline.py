273fd9fe0b63c8244bf080c11af5083e5069b25a
iyunbo <yunbo.wang@solven.eu>
2020-03-10 11:40:20 +0100

    setup for vscode env

--- workflow/pipeline.py
+++ workflow/pipeline.py
@@ -2,8 +2,8 @@ import mlflow
 
 from logparser import Drain
 
-input_dir = '../logs/raw/'  # The input directory of log file
-output_dir = 'result/'  # The output directory of parsing results
+input_dir = 'logs/raw/'  # The input directory of log file
+output_dir = 'workflow/result/'  # The output directory of parsing results
 log_file = 'log4j-2020-02-28-22.log'  # The input log file name
 log_format = '<Date> <Time> <Level> <Component>: <Content>'  # HDFS log format
 # Regular expression list for optional preprocessing (default: [])
