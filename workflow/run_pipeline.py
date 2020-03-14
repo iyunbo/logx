0bca2bd4346121a13a4de16dfe68cbf4ea0aa1b6
iyunbo <yunbo.wang@solven.eu>
2020-03-15 00:17:19 +0100

    minor improvement

--- workflow/run_pipeline.py
+++ workflow/run_pipeline.py
@@ -3,9 +3,11 @@ from workflow import parse, preprocess, train, predict
 
 def main():
     csv_file = parse.run()
+
     dataloader, num_classes = preprocess.run(csv_file)
+
     model_file = train.run(dataloader, num_classes)
-    print('model trained under: {}'.format(model_file))
+
     predict.run(num_classes, model_file)
 
 
