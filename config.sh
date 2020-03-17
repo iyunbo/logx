363002b63a85c841e3e33374e66944387bc0df64
iyunbo <yunbo.wang@solven.eu>
2020-03-17 23:16:00 +0100

    try to setup mlflow (not working)

---
+++ config.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# result of ‘which conda’ ending in “anaconda3”
+export MLFLOW_CONDA_HOME=/home/iyunbo/anaconda3
+# result of ‘pwd’ in project root plus “mlruns”
+# export MLFLOW_TRACKING_URI=”/Users/user/your/project/mlruns”
+# export MLFLOW_EXPERIMENT_NAME=”getting-started”
