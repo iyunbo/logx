9a8c74886db9da25516ef2894c5b91b7d0d887a3
iyunbo <yunbo.wang@solven.eu>
2020-03-20 11:59:28 +0100

    kedro (python3) works only under utf-8

--- config.sh
+++ config.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-# result of ‘which conda’ ending in “anaconda3”
-export MLFLOW_CONDA_HOME=/home/iyunbo/anaconda3
-# result of ‘pwd’ in project root plus “mlruns”
-# export MLFLOW_TRACKING_URI=”/Users/user/your/project/mlruns”
-# export MLFLOW_EXPERIMENT_NAME=”getting-started”
+echo setting up environment for en_US.UTF-8
+
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LANGUAGE=en_US.UTF-8
+
